128ab75544208e5c480df7b1f252b748651a9952
goefte <khairul.efte@gmail.com>
2025-10-30 22:05:37 +0600

    Ready for Render deployment

--- goefte/asgi.py
+++ goefte/asgi.py
@@ -8,9 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
 
-from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goefte.settings')
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'goefte_blog.settings')
-
-application = get_asgi_application()
+application = get_wsgi_application()
